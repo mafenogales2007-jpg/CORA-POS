c79a5640839cdbb330862f2c4c36c63890a262d2
hernandezgarzonadrianfelipe-arch <hernandezgarzonadrianfelipe@gmail.com>
2026-09-22 11:47:36 -0500

    Delete src directory

--- src/services/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://smvtfdisjufvdxgdljif.supabase.co';
-const supabaseAnonKey = 'sb_publishable_xS0APSPUzYxky9ZuuyHPUw_6fGdVFGR';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export default supabase;
--- src/services/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const URL = 'https://smvtfdisjufvdxgdljif.supabase.co';
-const KEY = 'sb_publishable_xS0APSPUzYxky9ZuuyHPUw_6fGdVFGR';
-
-export const supabase = createClient(URL, KEY);
-export default supabase;
